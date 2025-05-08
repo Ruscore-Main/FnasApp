971aa56581eba7e336f087400cb6d8b6a0db7db1
tugga346 <oobitovich@mail.ru>
2025-05-09 01:44:55 +0300

    fixed footer (LogoFooter.svg path)

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -8,7 +8,7 @@ export const Footer: React.FC = () => {
     <footer className={s.footer}>
       <Container>
         <div className={s.footer__logo}>
-          <Image src="/logoFooter.svg" alt="logoFooter" width={102} height={122} />
+          <Image src="/LogoFooter.svg" alt="logoFooter" width={102} height={122} />
         </div>
         <div className={s.footer__content}>
           <h2 className={s.footer__title}>Креативное агенство 500na700</h2>
